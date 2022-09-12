a1d27f27d2395e267e25cbdc95aafb6fc65297fe
Sébastian Le Merdy <567637+seblm@users.noreply.github.com>
2022-09-12 02:36:24 +0200

    :arrow_up: update dev.openfeature:javasdk, unleash-client-java and junit-jupiter

--- src/main/java/io/getunleash/openfeature/UnleashProvider.java
+++ src/main/java/io/getunleash/openfeature/UnleashProvider.java
@@ -19,18 +19,18 @@ public class UnleashProvider implements FeatureProvider {
     }
 
     @Override
-    public ProviderEvaluation<Boolean> getBooleanEvaluation(String key, Boolean defaultValue, EvaluationContext ctx, FlagEvaluationOptions options) {
+    public ProviderEvaluation<Boolean> getBooleanEvaluation(String key, Boolean defaultValue, EvaluationContext ctx) {
         boolean enabled = unleash.isEnabled(key);
         return ProviderEvaluation.<Boolean>builder().value(enabled).build();
     }
 
     @Override
-    public ProviderEvaluation<String> getStringEvaluation(String key, String defaultValue, EvaluationContext ctx, FlagEvaluationOptions options) {
+    public ProviderEvaluation<String> getStringEvaluation(String key, String defaultValue, EvaluationContext ctx) {
         return ProviderEvaluation.<String>builder().value(unleash.getVariant(key, new Variant(defaultValue, (String) null, false)).getName()).build();
     }
 
     @Override
-    public ProviderEvaluation<Integer> getIntegerEvaluation(String key, Integer defaultValue, EvaluationContext evaluationContext, FlagEvaluationOptions flagEvaluationOptions) {
+    public ProviderEvaluation<Integer> getIntegerEvaluation(String key, Integer defaultValue, EvaluationContext ctx) {
         String variantName = unleash.getVariant(key).getName();
         Integer value;
         try {
@@ -42,7 +42,19 @@ public class UnleashProvider implements FeatureProvider {
     }
 
     @Override
-    public <T> ProviderEvaluation<T> getObjectEvaluation(String key, T defaultValue, EvaluationContext invocationContext, FlagEvaluationOptions options) {
+    public ProviderEvaluation<Double> getDoubleEvaluation(String key, Double defaultValue, EvaluationContext ctx) {
+        String variantName = unleash.getVariant(key).getName();
+        Double value;
+        try {
+            value = Double.parseDouble(variantName);
+        } catch (NumberFormatException e) {
+            value = defaultValue;
+        }
+        return ProviderEvaluation.<Double>builder().value(value).build();
+    }
+
+    @Override
+    public ProviderEvaluation<Structure> getObjectEvaluation(String key, Structure defaultValue, EvaluationContext ctx) {
         throw new RuntimeException("Not implemented - Unleash does not support an 'Object' type. Only String, Number and Boolean");
     }
 
--- src/test/java/io/getunleash/openfeature/UnleashProviderTest.java
+++ src/test/java/io/getunleash/openfeature/UnleashProviderTest.java
@@ -1,12 +1,14 @@
 package io.getunleash.openfeature;
 
-import dev.openfeature.javasdk.Client;
-import dev.openfeature.javasdk.OpenFeatureAPI;
+import dev.openfeature.javasdk.*;
 import io.getunleash.FakeUnleash;
 import io.getunleash.Variant;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,18 +18,20 @@ public class UnleashProviderTest {
     private static Client client;
     private static UnleashProvider provider;
     private static FakeUnleash unleash;
+    private static OpenFeatureAPI api;
 
     @BeforeAll
-    static void createOpenFeatureClient() {
+    static void createOpenFeatureApiAndUleash() {
         unleash = new FakeUnleash();
         provider = new UnleashProvider(unleash);
-        OpenFeatureAPI api = OpenFeatureAPI.getInstance();
+        api = OpenFeatureAPI.getInstance();
         api.setProvider(provider);
         client = api.getClient();
     }
 
     @AfterEach
-    void resetUnleash() {
+    void resetOpenFeatureAPIAndUnleash() {
+        api.clearHooks();
         unleash.resetAll();
     }
 
@@ -60,8 +64,28 @@ public class UnleashProviderTest {
     }
 
     @Test
+    void mustResolveDoubleValue() {
+        unleash.enable("fib-value");
+        unleash.setVariant("fib-value", new Variant("8.0", (String) null, true));
+
+        assertEquals(8.0, client.getDoubleValue("fib-value", 0.0));
+    }
+
+    @Test
     void cantResolveObjectValue() {
-        assertThrows(RuntimeException.class, () -> client.getObjectValue("object", null));
+        final Exception[] errors = {null};
+        client.addHooks(new Hook<Structure>() {
+            @Override
+            public void error(HookContext<Structure> ctx, Exception error, Map<String, Object> hints) {
+                errors[0] = error;
+            }
+        });
+        Structure defaultValue = new Structure(Map.of("test", new Value(true)));
+        Structure result = client.getObjectValue("object", defaultValue);
+
+        assertEquals(result, defaultValue);
+        assertNotNull(errors[0]);
+        assertInstanceOf(RuntimeException.class, errors[0]);
     }
 
 }
